aa584ff3c517e1c9810632f54b1edcee14307de6
sebastian avella calderon <sebastian.avella@upb.edu.co>
2025-10-23 11:06:07 -0500

    clase 23

--- Archivos/leer_csv2.py
+++ Archivos/leer_csv2.py
@@ -4,8 +4,9 @@ with open("./Archivos/Variables..csv", 'r') as csvfile:   #usamos el manejador d
     encabezado = next(lector)   #Next salta la primera fila
     #print(encabezado)
     presion = []
-    print(encabezado[0])
+    print(encabezado[3])
     for fila in lector:  #con el for se itera sobre el objeto para leer
-        dato = int(fila[0])      
+        fila[3] = fila[3].replace(',', '.')     #replace sirve para modificar algo que tenga en algo nuevo
+        dato = float(fila[3])      
         presion.append(dato)  
 print(presion)
